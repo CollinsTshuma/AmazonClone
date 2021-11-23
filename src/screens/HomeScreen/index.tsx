ce2fc7a3b9b095391bcfee0e6b6c036d78365a7e
CollinsTshuma <62089415+CollinsTshuma@users.noreply.github.com>
2021-11-23 09:53:59 +0200

    feat: Build Product Item Component

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,39 +1,21 @@
-import React from 'react'
-import { View, Image, StyleSheet, Text } from 'react-native'
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import ProductItem from '../../components/ProductItem';
+import products from '../../data/products';
 
 const HomeScreen = () => {
-    return (
-        <View>
-            <View style={styles.root}>
-                <Image style={styles.image} source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleanarchitecture.jpg' }}/>
-                <View style={styles.rightContainer}>
-                    <Text style={styles.title} numberOfLines={3}>Logitech MX Master 3 Advanced Wireless Mouse for Mac - Bluetooth/USB</Text>
-                    {/*Ratings */}
-                    <Text style={styles.price}>from $13.59</Text>
-                </View>
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={styles.page}>
+        <ProductItem item ={products[0]}/>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    root:{
-        flexDirection: 'row',
-        margin: 10,
-        borderWidth: 1,
-        borderColor: '#d1d1d1',
-        borderRadius: 10,
-        backgroundColor: '#fff'
-    },
-    image:{
-        width: 150,
-        height: 150,
-    },
-    rightContainer: {
+    page: {
         padding: 10,
-    },
-    title: {},
-    price: {},
+    }
 })
 
-export default HomeScreen
+
+export default HomeScreen;
