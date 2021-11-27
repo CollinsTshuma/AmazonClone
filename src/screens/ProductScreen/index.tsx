50666ab2d9a5f219a48e574cb6e55b9ccfb30183
CollinsTshuma <62089415+CollinsTshuma@users.noreply.github.com>
2021-11-27 19:17:48 +0200

    feat: Work on the option selector in the ProductScreen

--- src/screens/ProductScreen/index.tsx
+++ src/screens/ProductScreen/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
 import product from '../../data/product';
 import {Picker} from '@react-native-picker/picker'
 
 const ProductScreen = () => {
+
+  const [selectedOption, setSelectedOption] = useState(
+    undefined,
+  );
+
   return (
     <View>
       <Text style={styles.title}>{product.title}</Text>
       {/*Image Corousel */}
 
       {/*Option Selector */}
+      <Picker
+        selectedValue={selectedOption}
+        onValueChange={itemValue => setSelectedOption(itemValue)}>
+        {product.options.map(option => (
+          <Picker.Item label={option} value={option} />
+        ))}
+      </Picker>
 
       {/*Price */}
       <Text style={styles.price}>
