2986c2b9fdc7c8894873f2e66595d6e5074136a2
CollinsTshuma <62089415+CollinsTshuma@users.noreply.github.com>
2021-12-05 21:34:56 +0200

    feat: Add navigation to the project using react navigation

--- App.tsx
+++ App.tsx
@@ -7,52 +7,26 @@
  *
  * @format
  */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-import ProductScreen from './src/screens/ProductScreen';
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ProductScreen/>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import 'react-native-gesture-handler';
+ import React from 'react';
+ import {SafeAreaView, View, StatusBar, useColorScheme} from 'react-native';
+ 
+ import {Colors} from 'react-native/Libraries/NewAppScreen';
+ import Router from './src/router';
+ 
+ const App = () => {
+   const isDarkMode = useColorScheme() === 'dark';
+ 
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+     flex: 1,
+   };
+   return (
+     <View style={backgroundStyle}>
+       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+       <Router />
+     </View>
+   );
+ };
+ 
+ export default App;
